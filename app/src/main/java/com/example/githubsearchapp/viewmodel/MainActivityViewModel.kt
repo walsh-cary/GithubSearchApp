7a0e94affa89e135f83a95401145537959c841c7
walsh-cary <walsh.cary.a@gmail.com>
2020-03-21 16:50:03 -0400

    MainActivityViewModel implemented

--- app/src/main/java/com/example/githubsearchapp/viewmodel/MainActivityViewModel.kt
+++ app/src/main/java/com/example/githubsearchapp/viewmodel/MainActivityViewModel.kt
@@ -1,9 +1,28 @@
 package com.example.githubsearchapp.viewmodel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.githubsearchapp.model.UserDetailResponse
+import com.example.githubsearchapp.model.UserSearchResponse
 
 class MainActivityViewModel(val baseURL: String) : ViewModel() {
-    private val userDataSet: MutableLiveData<UserDetailResponse> = MutableLiveData()
+    private val userSearchDataSet: MutableLiveData<UserSearchResponse> = MutableLiveData()
+    private val userDetailDataSet: MutableLiveData<UserDetailResponse> = MutableLiveData()
+
+    fun getUserDetailDataSet(): LiveData<UserDetailResponse> {
+        return userDetailDataSet
+    }
+
+    fun getUserSearchDataSet(): LiveData<UserSearchResponse> {
+        return userSearchDataSet
+    }
+
+    fun getUserSearchData(dataSet: UserSearchResponse) {
+        this.userSearchDataSet.value = dataSet
+    }
+
+    fun getUserDetailData(dataSet: UserDetailResponse) {
+        this.userDetailDataSet.value = dataSet
+    }
 }
